283be38c6dadd8ca85f8eb12366e7097adfae448
11968-ship-it <11968@holbertonstudents.com>
2025-10-19 20:16:46 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,18 @@
-#include ""
-#include <>
+/*malloc, free*/
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
 /*
 *
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-stuct createnode = 
+binary_tree_t *node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+return (NULL);
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
+return (node);
 }
