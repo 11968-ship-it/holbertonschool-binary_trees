cf842c5b3d96b0f97281c6b95d6f7a9d45a7fe89
root <11982@holbertonstudents.com>
2025-10-20 14:56:47 +0000

    Fix Betty style issues in 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,9 +3,9 @@
 
 /**
 * binary_tree_height - measures the height of a binary tree.
-* 
+*
 * @tree: a pointer to the root node of the tree.
-* 
+*
 * Return: the height of the binary tree,
 * if tree is NULL, return 0.
 */
@@ -13,18 +13,18 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-    size_t right_height = 0;
-    size_t left_height = 0;
+	size_t right_height = 0;
+	size_t left_height = 0;
 
-  if (tree == NULL)
-    return (0);
-    
-  if (tree->left)
-    left_height = 1 + binary_tree_height(tree->left);
-  if (tree->right)
-    right_height = 1 + binary_tree_height(tree->right);
+	if (tree == NULL)
+	return (0);
 
-  if (left_height > right_height)
-  return (left_height);
-  return(right_height);
+	if (tree->left)
+	left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+	right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	return (left_height);
+	return (right_height);
 }
