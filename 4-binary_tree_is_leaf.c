6110899099e65f0ba84c1f47518581a04ec037ca
11968-ship-it <11968@holbertonstudents.com>
2025-10-20 19:29:47 +0300

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,17 @@
-
+/*malloc, free*/
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+/*if it is a NULL not a leaf*/
+if (node == NULL)
+return (0);
+/*to check the childern*/
+if (node->left == NULL && node->right == NULL)
+return (1);
+/*not a leaf*/
+return (0);
+}
