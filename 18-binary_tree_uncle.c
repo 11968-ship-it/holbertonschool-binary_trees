2a3b9e68ab3c3fed0713ae89dabed35e7c0aedfd
11968-ship-it <11968@holbertonstudents.com>
2025-10-22 19:10:29 +0300

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "binary_trees.h"
+/*
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+binary_tree_t *grandparent;
+if (node == NULL || node->parent == NULL ||
+node->parent->parent == NULL)
+grandparent = node->parent->parent;
 
+}
