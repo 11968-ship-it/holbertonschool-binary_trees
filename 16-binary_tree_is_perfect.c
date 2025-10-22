8d09a2323d1daa49891e5219e074bfbb0919d9ef
root <11982@holbertonstudents.com>
2025-10-22 15:46:19 +0000

    Fixed Betty-style issues in 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,36 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * bt_height - Measures the height of a binary tree (local helper)
- * @tree: pointer to the root node of the tree to measure
- *
- * Return: height of the tree, or 0 if tree is NULL
- */
+* bt_height - Measures the height of a binary tree (local helper)
+* @tree: pointer to the root node of the tree to measure
+*
+* Return: height of the tree, or 0 if tree is NULL
+*/
 static size_t bt_height(const binary_tree_t *tree)
 {
-    size_t lh, rh;
+	size_t lh, rh;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    lh = tree->left ? 1 + bt_height(tree->left) : 0;
-    rh = tree->right ? 1 + bt_height(tree->right) : 0;
+	lh = tree->left ? 1 + bt_height(tree->left) : 0;
+	rh = tree->right ? 1 + bt_height(tree->right) : 0;
 
-    return (lh > rh ? lh : rh);
+return (lh > rh ? lh : rh);
 }
 
 /**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure
- *
- * Return: size (number of nodes), or 0 if tree is NULL
- */
+* binary_tree_size - Measures the size of a binary tree
+* @tree: pointer to the root node of the tree to measure
+*
+* Return: size (number of nodes), or 0 if tree is NULL
+*/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
